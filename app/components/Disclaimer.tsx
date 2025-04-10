1ff51fd081bb2ffa4820f825673c1b8fc8f1fd9e
leoantony2002 <leoantony20025@gmail.com>
2025-04-10 19:56:10 +0530

    v11

--- app/components/Disclaimer.tsx
+++ app/components/Disclaimer.tsx
@@ -21,11 +21,34 @@ const Disclaimer: React.FC = () => {
     setIsVisible(false);
     document.body.style.overflow = "unset";
   };
+
   if (!isVisible) return null;
 
   return (
     <div className="fixed inset-0 bg-white bg-opacity-20 backdrop-blur-sm flex items-center justify-center z-50">
-      <div className="text-center text-black p-8 rounded-2xl shadow-2xl max-w-sm w-full mx-4 border border-white bg-gradient-to-br from-[#dff0ff] to-[#ffced6]">
+      <div className="relative text-center text-black p-8 rounded-2xl shadow-2xl max-w-sm w-full mx-4 border border-white bg-gradient-to-br from-[#dff0ff] to-[#ffced6]">
+        {/* Close Icon */}
+        <button
+          onClick={handleClose}
+          className="absolute top-4 right-4 text-gray-600 hover:text-gray-800 transition"
+          aria-label="Close Disclaimer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+
         <h2 className="text-2xl font-bold mb-4">Disclaimer</h2>
         <p className="mb-4">
           At EB1A Visionary Path, we aim to share helpful insights for your
@@ -33,12 +56,6 @@ const Disclaimer: React.FC = () => {
           informational only and should not be considered legal advice or a
           substitute for professional legal guidance.
         </p>
-        <button
-          onClick={handleClose}
-          className="text-white px-10 py-4 mt-8 font-bold bg-gradient-to-r from-[#34A1FF] to-[#ff4747] rounded-full shadow-[-10px_20px_40px_var(--shadow1),10px_20px_40px_var(--shadow2)] transition duration-300"
-        >
-          Got it
-        </button>
       </div>
     </div>
   );
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -25,7 +25,7 @@ const navigation = {
     },
     {
       name: "Instagram",
-      href: "#",
+      href: "https://www.instagram.com/eb1avisionarypath",
       icon: (props: any) => (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -37,34 +37,12 @@ const navigation = {
         </svg>
       ),
     },
-    {
-      name: "Twitter",
-      href: "#",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-        </svg>
-      ),
-    },
-    {
-      name: "Facebook",
-      href: "#",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
   ],
 };
 
 export default function Footer() {
   return (
-    <footer className="relative flex justify-end max-h-[70vh] pt-28 overflow-visible pb-64">
+    <footer className="relative flex justify-end max-h-[85vh] lg:max-h-[75vh] pt-28 overflow-visible pb-64">
       <Image
         src={footer_bg}
         alt=""
@@ -92,6 +70,7 @@ export default function Footer() {
             <a
               key={item.name}
               href={item.href}
+              target="_blank"
               className="opacity-55 hover:opacity-100"
             >
               <span className="sr-only">{item.name}</span>
@@ -99,7 +78,7 @@ export default function Footer() {
             </a>
           ))}
         </div>
-        <div className="mt-10 flex justify-center space-x-10 text-sm leading-6">
+        <div className="mt-10 flex flex-wrap lg:flex-nowrap gap-5 justify-center space-x-10 text-sm leading-6">
           <a
             href="tel:+918248633492"
             className="flex items-center opacity-55 hover:opacity-100"
@@ -133,11 +112,13 @@ export default function Footer() {
           &copy; {new Date().getFullYear()} EB1A Visionary Path. All rights
           reserved.
         </p>
-        <div className="mt-10 text-center text-xs leading-5">
+        <div className="absolute bottom-0 left-0 w-screen mt-10 p-5 bg-white/40 text-center text-xs leading-5">
           <p>
-            EB1A Visionary Path is not a law firm and does not provide legal
-            advice. Our services are limited to profile building and evidence
-            collection.
+            <strong>Disclaimer: </strong> At EB1A Visionary Path, we aim to
+            share helpful insights for your journey, but please note that we are
+            not a law firm. Our content is informational only and should not be
+            considered legal advice or a substitute for professional legal
+            guidance.
           </p>
         </div>
       </div>
